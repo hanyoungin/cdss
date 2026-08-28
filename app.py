685f36dcc7718e80f04fd6d0328693fc0c90791b
한영인 <38682246+hanyoungin@users.noreply.github.com>
2026-08-28 20:47:27 +0900

    Update demo.launch to use environment variable for port

--- app.py
+++ app.py
@@ -1912,4 +1912,8 @@ with gr.Blocks(
     )
 
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=7860, show_error=True)
+    demo.launch(
+        server_name="0.0.0.0",
+        server_port=int(os.environ.get("PORT", 7860)),
+        show_error=True,
+    )
