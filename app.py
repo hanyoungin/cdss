08b658456a1087dbfa7241ccbb95d74dffa1c30f
한영인 <38682246+hanyoungin@users.noreply.github.com>
2026-08-28 21:08:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1914,6 +1914,6 @@ with gr.Blocks(
 if __name__ == "__main__":
     demo.launch(
         server_name="0.0.0.0",
-        server_port=int(os.environ.get("PORT", 7860)),
+        server_port=int(os.environ.get("PORT", 8080)),
         show_error=True,
     )
